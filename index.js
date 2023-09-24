ee13edaac67c8d08cf181dc6a7409f89ad7d3e1d
Andy18j <115467259+Andy18j@users.noreply.github.com>
2023-09-24 16:42:54 +0530

    asdf

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const express = require("express")
 require("dotenv").config()
 const {connection}=require("./config/db")
 const {bookRouter} = require("./routes/book.routes")
+const cors =require("cors")
+
+
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 
 app.use("",bookRouter)
